f37184ab6945e3bebab6445b7b6a56b57ea98849
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-18 12:26:39 -0200

    Uses the Hibernate @Id annotation to map the id value to the Database

---
+++ hibernate-generation-type-strategies/README.md
@@ -0,0 +1,3 @@
+[![Stories in Ready](https://badge.waffle.io/craft-coder/hibernate-guide-tutorials.png?label=ready&title=Ready)](https://waffle.io/craft-coder/hibernate-guide-tutorials)
+# hibernate-guides-tutorials
+Craft Coder - Guides and Tutorials
---
+++ hibernate-generation-type-strategies/src/main/java/com/craft/coder/hibernate/infra/HibernateConnection.java
@@ -0,0 +1,49 @@
+package com.craft.coder.hibernate.infra;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+
+import com.craft.coder.hibernate.mapping.Employee;
+
+public class HibernateConnection {
+
+	private static SessionFactory sessionFactory;
+	
+	public static Session getSession() {
+		createSessionFactory();
+		
+		return sessionFactory.openSession();
+	}
+	
+	public static EntityManager getEntityManager() {
+		createSessionFactory();
+		
+		return sessionFactory.createEntityManager();
+	}
+
+	private static void createSessionFactory() {
+		if (sessionFactory == null) {
+			Configuration cfg = new Configuration()
+					.setProperty(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL5InnoDBDialect")
+					.setProperty(AvailableSettings.DRIVER, "com.mysql.jdbc.Driver")
+				    .setProperty(AvailableSettings.URL, "jdbc:mysql://localhost:3306/hibernate-guide-tutorials")
+				    .setProperty(AvailableSettings.USER, "root")
+				    .setProperty(AvailableSettings.PASS, "")
+				    .setProperty(AvailableSettings.HBM2DDL_AUTO, "none")
+				    .setProperty(AvailableSettings.SHOW_SQL, "true")
+				    .setProperty(AvailableSettings.FORMAT_SQL, "true")
+				    .addAnnotatedClass(Employee.class);
+	
+			sessionFactory = cfg.buildSessionFactory();
+		}
+	}
+	
+	public static void closeFactory() {
+		sessionFactory.close();
+	}
+	
+}
