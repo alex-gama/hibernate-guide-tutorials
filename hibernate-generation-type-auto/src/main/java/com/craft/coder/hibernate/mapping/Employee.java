227df6ecfbc9ff9455122a096b71b2bcef0d96e5
Alexandre Gama <alexandre.gama.lima@gmail.com>
2017-02-18 15:52:13 -0200

    Using Hibernate GenerationType.AUTO to generate Ids automatically

--- hibernate-generation-type-auto/src/main/java/com/craft/coder/hibernate/mapping/Employee.java
+++ hibernate-generation-type-auto/src/main/java/com/craft/coder/hibernate/mapping/Employee.java
@@ -2,13 +2,14 @@ package com.craft.coder.hibernate.mapping;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Employee {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String firstName;
